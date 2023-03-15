2194376a97db3a6b4980ebecd820d482af7d9bb7
MarsMwau <marthamumbua16@gmail.com>
2023-03-15 13:14:46 +0300

    Done with this

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    callback(); // call the callback function
+  }
+function myCallBack() {
+    Return `Hello World`;
+}
+receivesAFunction(myCallBack); 
+
+function returnsANamedFunction() {
+    return function named() {
+        console.log(`Am a named function!`);
+    }
+}
+function returnsAnAnonymousFunction() {
+    return function() {}
+}
